05d5c7eeda8e7d103f025f38fd66dc2e67595de4
KARTHIKPROJECT <88829860+KARTHIKPROJECT@users.noreply.github.com>
2021-08-12 17:57:07 +0530

    Revert back
    Revert Back Keyboard Settings to original

--- Firstproject.java
+++ Firstproject.java
@@ -4,7 +4,7 @@ public class Calculator {
 public static void main(String[] args) {
 Scanner reader = new Scanner(System.in);
 System.out.print("Enter two numbers: ");
-// nextDouble() reads the next double from the keyboard And Shortcut Create"R"
+// nextDouble() reads the next double from the keyboard
 double first = reader.nextDouble();
 double second = reader.nextDouble();
 System.out.print("Enter an operator (+, -, *, /): ");
